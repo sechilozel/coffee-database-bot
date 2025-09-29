5a12d32855e0a51d7c90a5a56a0552b4c07dd538
Seçil ÖZEL <105538435+sechilozel@users.noreply.github.com>
2025-09-29 21:27:00 +0300

    Commit 1

---
+++ botv2.py
@@ -0,0 +1,58 @@
+import discord
+from discord.ext import commands
+import pandas as pd
+from thefuzz import process
+from logic import DB_Manager
+from config import TOKEN, DATABASE
+
+manager = DB_Manager(DATABASE)
+
+intents = discord.Intents.default()
+intents.message_content = True  
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+df = pd.read_csv("coffeee.csv")
+df['keywords'] = df['Kahve Tipi'].str.lower().str.split()  
+
+def guess_coffee(user_input, df, threshold=70):
+    user_input = user_input.lower()
+    choices = df['Kahve Tipi'].tolist() + [' '.join(k) for k in df['keywords']]
+    best_match = process.extractOne(user_input, choices)
+    
+    if best_match[1] >= threshold:
+       
+        recipe_row = df[df['Kahve Tipi'] == best_match[0]]
+        if not recipe_row.empty:
+            return recipe_row.iloc[0].to_dict()
+   
+        for i, kws in enumerate(df['keywords']):
+            if best_match[0] == ' '.join(kws):
+                return df.iloc[i].to_dict()
+    return None
+
+
+@bot.command()
+async def kahve(ctx, *, message):
+    result = guess_coffee(message, df)
+    if result:
+        # message.lower()
+        # await ctx.send(manager.give_coffee)
+        # response = "\n".join([f"{k}: {v}" for k, v in result.items()])
+        response = "\n".join([f"{k}: {v}" for k, v in result.items() if k != 'keywords'])
+    else:
+        user_input_lower = message.lower()
+        choices = df['Kahve Tipi'].tolist() + [' '.join(k) for k in df['keywords']]
+        best_match = process.extractOne(user_input_lower, choices)
+        
+        if best_match[1] >= 20:
+            # Sadece tahmin edilen kahve ismini göster
+            await ctx.send(f"İstediğiniz kahve bu olabilir mi? -> {best_match[0]}")
+        else:
+            await ctx.send("Üzgünüm, bu kahveyi bulamadım.")
+        # response = "Üzgünüm, bu kahveyi bulamadım."
+    await ctx.send(response)
+
+
+bot.run(TOKEN)
